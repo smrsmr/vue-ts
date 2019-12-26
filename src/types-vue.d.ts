4ba54599cc94e585912161be4598e1d137ab031d
shenmaorui <1246054720@qq.com>
2019-12-26 16:34:32 +0800

    add

--- src/types-vue.d.ts
+++ src/types-vue.d.ts
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import { Route } from "vue-router";
-import { Store } from 'vuex';
+import { Store } from "vuex";
 //原因是ts没法自动推导出vue原型上的属性，需要声明一下
 declare module "vue/types/vue" {
   interface Vue {
     $api: any;
     $config: any;
     $router: VueRouter;
-		$route: Route;
-		$store: Store<any>;
+    $route: Route;
+    $store: Store<any>;
   }
 }
